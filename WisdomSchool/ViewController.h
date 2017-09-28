75afc11cfc6bc703f4477fa89f6f4fb22c551606
Resuming <Cc@weiChendeMacBook-Pro.local>
2017-09-29 00:57:27 +0800

    Initial Commit

---
+++ WisdomSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WisdomSchool
+//
+//  Created by Resuming on 2017/9/29.
+//  Copyright © 2017年 Resuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
